9086342aa36b9404f7de5a968195112212ab8439
Eliseo Soto <eliseo.soto@motorola.com>
2012-03-04 00:54:12 -0800

    Bound the selected path to the SELECTED_DIRECTORY preference. Fixes #1

--- src/com/supervaca/wallpaperChanger/WallpaperPreferencesActivity.java
+++ src/com/supervaca/wallpaperChanger/WallpaperPreferencesActivity.java
@@ -1,5 +1,8 @@
 package com.supervaca.wallpaperChanger;
 
+import com.lamerman.FileDialogWithDirectories;
+import com.lamerman.SelectionMode;
+
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Intent;
@@ -8,15 +11,20 @@ import android.os.Bundle;
 import android.os.SystemClock;
 import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
 import android.util.Log;
 
 public class WallpaperPreferencesActivity extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
-    public static final String SERVICE_ACTIVE = "SERVICE_ACTIVE";
+    private static final int SELECT_DIRECTORY_RESULT = 777;
+	public static final String SERVICE_ACTIVE = "SERVICE_ACTIVE";
     public static final String UPDATE_INTERVAL = "UPDATE_INTERVAL";
+    public static final String SELECTED_DIRECTORY = "SELECTED_DIRECTORY";
 
     private ListPreference updateIntervalListPref;
     private CheckBoxPreference serviceActiveCheckBoxPref;
+    private Preference selectedDirectoryPreference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,20 +36,39 @@ public class WallpaperPreferencesActivity extends PreferenceActivity implements 
         // Get a reference to the preferences
         updateIntervalListPref = (ListPreference) getPreferenceScreen().findPreference(UPDATE_INTERVAL);
         serviceActiveCheckBoxPref = (CheckBoxPreference) getPreferenceScreen().findPreference(SERVICE_ACTIVE);
+        selectedDirectoryPreference = getPreferenceScreen().findPreference(SELECTED_DIRECTORY);
+        
+        // Manually add a listener to selectDirectoryPreference, this is the only way to
+        // invoke startActivityForResult and pass the right extras
+        OnPreferenceClickListener opcl = new OnPreferenceClickListener() {
+			@Override
+			public boolean onPreferenceClick(Preference preference) {
+				Intent intent = new Intent(getBaseContext(), FileDialogWithDirectories.class);
+                intent.putExtra(FileDialogWithDirectories.CAN_SELECT_DIR, true);
+                intent.putExtra(FileDialogWithDirectories.SELECTION_MODE, SelectionMode.MODE_OPEN);
+                
+                startActivityForResult(intent, SELECT_DIRECTORY_RESULT);
+                
+				return true;
+			}
+		};
+        selectedDirectoryPreference.setOnPreferenceClickListener(opcl);
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
+	protected void onResume() {
+		super.onResume();
 
-        SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+		SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
 
-        // Setup the initial values
-        updateIntervalListPref.setSummary(updateIntervalListPref.getEntry());
+		// Setup the initial values
+		updateIntervalListPref.setSummary(updateIntervalListPref.getEntry());
+		selectedDirectoryPreference.setSummary(sharedPreferences.getString(SELECTED_DIRECTORY,
+				getText(R.string.directory_selection_none_selected).toString()));
 
-        // Set up a listener whenever a key changes
-        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
-    }
+		// Set up a listener whenever a key changes
+		sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+	}
 
     @Override
     protected void onPause() {
@@ -74,6 +101,24 @@ public class WallpaperPreferencesActivity extends PreferenceActivity implements 
                 // Cancel the alarm
                 am.cancel(pendingIntent);
             }
+        } else if (key.equals(SELECTED_DIRECTORY)) {
+        	selectedDirectoryPreference.setSummary(sharedPreferences.getString(SELECTED_DIRECTORY,
+    				getText(R.string.directory_selection_none_selected).toString()));
         }
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    	if(SELECT_DIRECTORY_RESULT == requestCode) {
+    		// The path selected in the dialog
+			String resultPath = data.getStringExtra(FileDialogWithDirectories.RESULT_PATH);
+			
+			// We need to listen for changes
+			SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+			sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+			
+			// Update the property with the resultPath
+			selectedDirectoryPreference.getEditor().putString(SELECTED_DIRECTORY, resultPath).commit();
+    	}
     }
 }
